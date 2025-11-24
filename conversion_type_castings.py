48a8f19cf49371ba1fb1aef1573f8744c3b3ff66
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-11-24 20:08:44 +0530

    conversions_type_casting

---
+++ conversion_type_castings.py
@@ -0,0 +1,9 @@
+num=5464
+print("Number in decimal form is : ",num)
+binary=bin(num)
+print("Number in binary form is : ",binary)
+octal=oct(num)
+print("Number in octal form is : ",octal)
+hexa=hex(num)
+print("Number in hexa decimal form is : ",hexa)
+
