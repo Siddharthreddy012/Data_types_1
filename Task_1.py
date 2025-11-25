47d9450811f18434208cb0ef5ccff45a04f52838
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-11-25 18:59:18 +0530

    type_casting_conversions

---
+++ Task_1.py
@@ -0,0 +1,25 @@
+num_1=8985 
+num_2=77777
+num_3=205
+num_4=7619
+num_5=73375
+print("Decimal form of num_1 is : ",num_1)
+print("Binary form of num_1 is : ",bin(num_1))
+print("Octal form of num_1 is : ",oct(num_1))
+print("Hexa decimal form of num_1 is : ",hex(num_1))
+print("Decimal form of num_2 is : ",num_2)
+print("Binary form of num_2 is : ",bin(num_2))
+print("Octal form of num_2 is : ",oct(num_2))
+print("Hexa decimal form of num_2 is : ",hex(num_2))
+print("Decimal form of num_3 is : ",num_3)
+print("Binary form of num_3 is : ",bin(num_3))
+print("Octal form of num_3 is : ",oct(num_3))
+print("Hexa decimal form of num_3 is : ",hex(num_3))
+print("Decimal form of num_4 is : ",num_4)
+print("Binary form of num_4 is : ",bin(num_4))
+print("Octal form of num_4 is : ",oct(num_4))
+print("Hexa decimal form of num_4 is : ",hex(num_4))
+print("Decimal form of num_5 is : ",num_5)
+print("Binary form of num_5 is : ",bin(num_5))
+print("Octal form of num_5 is : ",oct(num_5))
+print("Hexa decimal form of num_5 is : ",hex(num_5))
